92879d97816d64176ae664629c5fa3d5ca54ef5d
grojas4 <grojas4@mail.csuchico.edu>
2017-10-02 16:37:30 -0700

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+// Gerardo Rojas
+#include<iostream>
+#include<string>
+#include"Bitmap.h"
+#include<vector>
+
+
+
+using namespace std;
+
+int main()
+{
+  // Ask user for an image name
+
+  
+  //convert image pixels to grayscale (average of RGB)
+  
+ 
+  //save file as oldtimey.bmp
+
+  
+  //Make sure that the image is readable if not let the user know that the file must be 24 bit  BMP file. ask to renter the image name 
+
+
+  return 0;
+}
+
